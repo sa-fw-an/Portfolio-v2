41f66f2c2968d286173c70fc09b98c1459c782fd
Safwan Sayeed <isafwansayeed@gmail.com>
2025-08-31 11:51:16 +0530

    refactor: Update ESLint ignore patterns and clean up HTML structure for improved readability

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,11 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    'public/assets/draco/**/*',
+    'node_modules/**/*'
+  ]),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
